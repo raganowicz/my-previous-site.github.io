12f4c0c8bd0acc3bfc1a445b04ccbbfebefe5538
piotrrm95 <piotr.rm95@gmail.com>
2019-10-10 23:10:12 +0200

    First commit

---
+++ components/videoController.js
@@ -0,0 +1,10 @@
+video_count =1;
+videoPlayer = document.getElementById("myVideo");
+
+function playMovie(){
+        video_count++;
+        if (video_count == 4) video_count = 1;
+        let nextVideo = "video/video"+video_count+".mp4";
+        videoPlayer.src = nextVideo;
+        videoPlayer.play();
+   };
