43dcac7450c0ac51b338e8c0673742907ec44639
piotrrm95 <piotr.rm95@gmail.com>
2019-10-16 12:30:51 +0200

    try to prevent scroll on mobile contact inputs

--- components/contact.js
+++ components/contact.js
@@ -1,10 +1,15 @@
+const emailInp = document.getElementById("email");
+const messageInp = document.getElementById("message");
+
+emailInp.focus({ preventScroll: true });
+messageInp.focus({ preventScroll: true });
+
 $(document).ready(function() {
   $(".submit").click(function(event) {
-    const email = $(".email").val();
-    const message = $(".message").val();
-    const status = $(".status");
+    const email = $("#email").val();
+    const message = $("#message").val();
+    const status = $("#status");
     status.empty();
-
     if (email.length > 5 && email.includes("@") && email.includes(".")) {
       status.append("");
     } else {
